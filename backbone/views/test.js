b198e880a2bd838e406d8076463c845b6404803a
janey <janeyhjy@gmail.com>
2014-03-28 17:53:30 +0800

    Update

--- backbone/views/test.js
+++ backbone/views/test.js
@@ -1,8 +1,15 @@
 define(['backbone', 'text!templates/test.html'], function(Backbone, TestTmpl) {
     var TestView = Backbone.View.extend({
         el: $("#container"),
+        template: TestTmpl,
+        events: {
+            
+        }
         initialize: function() {
             console.log(1);
+        },
+        render: function() {
+            
         }
     });
     return TestView;
