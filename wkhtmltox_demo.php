c06aed4c21bf0fd223a3f7952cfe9273e17fa458
janeyhjy <978214824@qq.com>
2017-09-04 16:51:09 +0800

    Add files via upload

---
+++ wkhtmltox_demo.php
@@ -0,0 +1,16 @@
+<?php
+/*
+需下载wkhtmltox工具: https://github.com/wkhtmltopdf/wkhtmltopdf
+依赖需要安装: https://github.com/openlabs/docker-wkhtmltopdf/blob/master/Dockerfile#L9-L11
+linux中文乱码: windows c:\Windows\fonts\simsun.ttc拷贝到linux服务器/usr/share/fonts/
+*/
+$wkhtmltoimage = '/wkhtmltox/bin/wkhtmltoimage';
+$filename = '/temp/' . uniqid('html2img_', false) . '.png';
+$htmlfilename = '/temp/' . uniqid('html2img_', false) . '.html';
+$htmlfilename = '/temp/111.html';
+$html = '<html><head><title>export</title><body>content</body></html>';
+// $reult = file_put_contents($htmlfilename, $html);
+$command =  $wkhtmltoimage . ' --width 640 --height 950 --quality 100' . escapeshellarg($htmlfilename) . ' ' . escapeshellarg($filename) . ' 2>&1' ;
+exec($command, $output, $return_val);
+// unlink($htmlfilename);
+print_r([$output, $return_val, $wkhtmltoimage, $htmlfilename, $filename]);die;
